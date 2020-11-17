ffa070de69dcdf3835ae110e205574427f877a8c
valeriö <valeriodambrosio.14@gmail.com>
2020-11-17 12:55:29 +0100

    Stampo a schermo i primi 20 risultati

---
+++ app/Http/Controllers/ArticleController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+// use App\Article;
+
+
+class ArticleController extends Controller
+{
+  public function index(){
+
+    $articles = json_decode(file_get_contents(config('services.newsapi.baseurl')
+    . 'everything?qInTitle=laravel&apiKey='
+    . config('services.newsapi.key')), true);
+
+    return view('welcome', compact('articles'));
+
+
+  }
+}
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,5 +1,45 @@
 @extends('layouts.app')
 @section('content')
 
+  @foreach($articles['articles'] as $article)
+    <div class="container">
+      <div class="row">
+        <div class="col">
+          <article>
+
+            <a href="{{ $article['url'] }}" target="_blank">
+
+              <h1>{{ $article['title'] }} </h1>
+              <img src="{{ $article['urlToImage'] }}">
+
+            </a>
+
+            <p>{{ $article['description'] }} </p>
+
+            <div>
+              Author: {{ $article['author'] ? : "Unknown" }}
+            </div>
+
+            @if($article['publishedAt'] !== null)
+              <div>
+                Date Published: {{ Carbon\Carbon::parse($article['publishedAt'])->format('l jS \\of F Y ') }}
+              </div>
+            @else
+              <div>
+                Date Published: Unknown
+              </div>
+            @endif
+
+          </article>
+
+        </div>
+
+      </div>
+
+    </div>
+    <br>
+    <br>
+    <br>
+  @endforeach
 
 @endsection
